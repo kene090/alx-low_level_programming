2f9c12a433b47bcf9c0f708961dddc7fe544dbe7
kene090 <eluik@yahoo.com>
2023-06-20 19:02:31 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,18 @@
 
 
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+char letter;
+
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+
+_putchar('\n');
 }
